57b23fc27ad0f7ff5b249a3ac75fe196ee6028ea
user <user@user-Inspiron-15-3565>
2020-04-15 22:41:03 +0530

    firstcommit

---
+++ stsintg/src/stsintg/billChk.java
@@ -0,0 +1,11 @@
+package stsintg;
+
+public class billChk {
+
+	public static void main(String args[])
+	{
+		System.out.println("hello world");
+		System.out.println("chekc");
+		System.out.println("update");
+	}
+}
